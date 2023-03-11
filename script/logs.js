547a2347f32556e1aa6b8072f821a693a834696b
kerry jalal <khariy34@hotmail.com>
2023-03-11 15:47:12 +0100

    update round

--- script/logs.js
+++ script/logs.js
@@ -1,5 +1,8 @@
 import { allGamesLog } from "./game.js";
 import { menu } from './menu.js'
+
+let gameCount = 0;  
+
 const get_choices = (i) =>{
 let message = '';
 for (let j = 0; j < 5 ; j++) {
@@ -9,13 +12,13 @@ return message;
 }
 ,logs = () =>{
 
-let gameCount = 0;    
+   
 
 for (let i = 0; i<allGamesLog.length; i++){
-    gameCount++
+       gameCount++
 
     alert(`${allGamesLog[i].winner}\n\t Game counts:${gameCount}.\n${allGamesLog[i].userName}: ${allGamesLog[i].userScore}\ncomputer: ${allGamesLog[i].computerScore}\n-----------------------------------------------------------------\n${allGamesLog[i].userName} I computer\n-----------------------------------------------------------------\n${get_choices(i)}-----------------------------------------------------------------`)
-
+ 
 }
 
 return menu();
